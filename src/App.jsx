a6bf5ed5e4536e45ccdedc080ff096891776e226
Kevin0403 <kevinsangani99247@gmail.com>
2024-03-14 00:32:57 +0530

    Footer added and styles are changed

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Toaster position="top-right" richColors />
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 bg-common">
         {/* <Sidebar /> */}
         <Outlet />
       </main>
